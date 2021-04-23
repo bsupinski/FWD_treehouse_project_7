8196da1d34ea838602775e2be92cd8c784a271cf
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-04-23 18:05:57 -0400

    almost done with mobile

--- js/clickEvents.js
+++ js/clickEvents.js
@@ -1,34 +1,31 @@
 trafficBar = document.querySelectorAll(".traffic-nav-link");
+const updateChart = (chart, newData) => {
+    chart.data.labels = newData.labels;
+    chart.data.datasets[0].data = newData.datasets[0].data;
+    chart.update({
+      duration: 800,
+      easing: 'linear',
+    });
+  };
 
 trafficBar.forEach(navItem => {
     navItem.addEventListener("click", function(){
         trafficBar.forEach(nav => nav.classList.remove('active'));
         this.classList.add('active');
         if (navItem.textContent === "Hourly" ){
-            const updateChart = (trafficChart, hourlyTrafficData) => {
-                trafficChart.data.datasets[0].data = dailyTrafficData.datasets[0].data;
-                trafficChart.update();
-              };
+            updateChart(trafficChart, trafficDataHourly);
+            
         }
         else if (navItem.textContent === "Daily" ){
-            const updateChart = (trafficChart, dailyTrafficData) => {
-                trafficChart.data.datasets[0].data = dailyDataHourly.datasets[0].data;
-                trafficChart.update();
-              };
+            updateChart(trafficChart, trafficDataDaily);
             
         }
         else if (navItem.textContent === "Weekly" ){
-            const updateChart = (trafficChart, weeklyTrafficData) => {
-                trafficChart.data.datasets[0].data = weeklyDataHourly.datasets[0].data;
-                trafficChart.update();
-              };
+            updateChart(trafficChart, trafficDataWeekly);
             
         }
         else if (navItem.textContent === "Monthly" ){
-            const updateChart = (trafficChart, monthlyTrafficData) => {
-                trafficChart.data.datasets[0].data = trafficDataHourly.datasets[0].data;
-                trafficChart.update();
-              };  
+            updateChart(trafficChart, trafficDataMonthly);
         }
     });
 });
