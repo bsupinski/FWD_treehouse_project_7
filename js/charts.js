31a9f6f61d89233d435026282ff5a4bd353a1b78
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-04-26 14:38:18 -0400

    figuring out text inside custom checkbox

--- js/charts.js
+++ js/charts.js
@@ -6,6 +6,15 @@ let mobileCanvas = document.getElementById("mobile-chart");
 //Chart inputs
 
 //Traffic chart variabes
+let trafficDataDefault = {
+    labels: ["13-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3","4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: "rgba(116, 119, 191, .3)",
+        borderWidth: 1
+    }]
+};
+
 let trafficDataHourly = {
     labels: ["13-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3","4-10", "11-17", "18-24", "25-31"],
     datasets: [{
@@ -114,7 +123,7 @@ const mobileOptions= {
 //Charts
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
-    data: trafficDataHourly,
+    data: trafficDataDefault,
     options: trafficOptions
 });
 
